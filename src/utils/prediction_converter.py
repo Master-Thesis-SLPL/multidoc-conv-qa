8b58cddb172ba384ff85f5a26ae7a8a247104a04
alistvt <alistvt@gmail.com>
2022-09-29 16:49:27 +0000

    fix

--- src/utils/prediction_converter.py
+++ src/utils/prediction_converter.py
@@ -6,7 +6,10 @@ this file removes all the other utterances and only keeps the last one.
 
 import json
 
-with open("predictions.json", 'r') as f:
+input_file = "../../results/dr_fud/predictions_mdd_dev_ids.json"
+output_file = input_file[:-5] + "_out.json"
+
+with open(input_file, 'r') as f:
     data = f.read()
 
 data = json.loads(data)
@@ -15,7 +18,7 @@ pr = {}
 for id_, val in data.items():
     x = id_.split('_')
     id = f"{x[0]}_{x[1]}"
-    turn_id = int(x[2])
+    turn_id = int(x[1])
     if id not in pr:
         pr[id] = {
             "id": id,
@@ -36,5 +39,5 @@ my_list = [value for key,value in pr.items()]
 
 json_obj = json.dumps(my_list, indent=2)
 
-with open("predicted.json", "w", encoding="utf-8") as f:
+with open(output_file, "w", encoding="utf-8") as f:
     f.write(json_obj)
